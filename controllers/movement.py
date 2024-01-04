88c785b37ab5430539656390cc10f864c72dcfba
outsidermm <outsidermm1234@gmail.com>
2024-01-05 00:46:37 +0800

    commented out later necessary methods and imports

--- controllers/movement.py
+++ controllers/movement.py
@@ -26,7 +26,8 @@ from wpimath.controller import (
 from wpimath.spline import Spline3
 import math
 from wpilib import Field2d
-from utilities.game import is_red
+
+# from utilities.game import is_red
 
 from utilities.functions import clamp
 
@@ -237,9 +238,9 @@ class Movement(StateMachine):
         override: override driver lockout
         """
         if not self.inputs_lock or override:
-            if is_red():
-                vx = -vx
-                vy = -vy
+            # if is_red():
+            #     vx = -vx
+            #     vy = -vy
 
             self.driver_inputs = (vx, vy, vz)
             self.drive_local = local
