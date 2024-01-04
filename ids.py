76d50338835e40d33cd9ef24dbd3566ec9555419
clubpenguin <amber.knights@thedropbears.org.au>
2024-01-05 00:36:01 +1100

    added ids

---
+++ ids.py
@@ -0,0 +1,23 @@
+import enum
+
+
+@enum.unique
+class TalonIds(enum.IntEnum):
+    drive_1 = 1
+    steer_1 = 5
+
+    drive_2 = 2
+    steer_2 = 6
+
+    drive_3 = 3
+    steer_3 = 7
+
+    drive_4 = 4
+    steer_4 = 8
+
+@enum.unique
+class CancoderIds(enum.IntEnum):
+    swerve_1 = 1
+    swerve_2 = 2
+    swerve_3 = 3
+    swerve_4 = 4
