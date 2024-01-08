2f1eb3cb13e09963847da4956df7c300b6826d77
outsidermm <outsidermm1234@gmail.com>
2024-01-08 11:19:12 +1100

    added method for module current in swervemod

--- components/chassis.py
+++ components/chassis.py
@@ -143,6 +143,9 @@ class SwerveModule:
         """Get the steer angle as a Rotation2d"""
         return Rotation2d(self.get_angle_integrated())
 
+    def get_module_current(self) -> float:
+        return self.drive.get_stator_current().value
+
     def get_speed(self) -> float:
         # velocity is in counts / 100ms, return in m/s
         return self.drive.get_rotor_velocity().value * self.DRIVE_COUNTS_TO_METRES * 10
@@ -422,7 +425,7 @@ class Chassis:
 
     @feedback
     def get_drive_current(self) -> float:
-        return sum(abs(x.drive.get_stator_current().value) for x in self.modules)
+        return sum(abs(x.get_module_current()) for x in self.modules)
 
     @feedback
     def may_be_stalled(self) -> bool:
