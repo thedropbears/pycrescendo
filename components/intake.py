79da3248e6dd536b2b7c6991e0a976fffb08c5b6
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-13 11:30:45 +1100

    Made intake class more general, and added things brought up by the PR

--- components/intake.py
+++ components/intake.py
@@ -1,9 +1,7 @@
 from magicbot import tunable
-import wpilib
 
 
-class Intake:
-    shooter_motor: wpilib.Talon
+class Motor:
     shoot_speed = tunable(1.0)  # speed is tunable via NetworkTables
 
     def __init__(self):
@@ -13,12 +11,26 @@ class Intake:
         self.deployed = True
 
     def is_ready(self):
+        # Check if the motor is ready to shoot
         return True
+
+    def is_note_present(self):
+        # Check if the note is in the intake
+        return False
+
+    def is_fully_retracted(self):
+        # Check if the intake is fully retracted
+        return True
+
+    def is_fully_deployed(self):
+        # Check if the intake is fully deployed
+        return False
 
     def execute(self):
         if self.deployed:
-            self.shooter_motor.set(self.shoot_speed)
+            # set the motor to the shoot_speed
+            pass
         else:
-            self.shooter_motor.set(0)
-
+            # set the motor speed to 0
+            pass
         self.deployed = False
