3d47f6d9e9123d9d51d47d3bb177c68c048711a6
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-13 11:36:26 +1100

    Added syntax annotation and removed unnecesary things from climb.py

--- components/climb.py
+++ components/climb.py
@@ -1,26 +1,13 @@
-from magicbot import tunable
-import wpilib
-
-
 class Climber:
-    shooter_motor: wpilib.Talon
-    shoot_speed = tunable(1.0)  # speed is tunable via NetworkTables
-
-    def __init__(self):
+    def __init__(self) -> None:
         self.deployed = False
 
-    def deploy(self):
-        """Causes the climber to do it's thing"""
+    def deploy(self) -> None:
         self.deployed = True
 
-    def is_ready(self):
-        return True
-
-    def execute(self):
-        """This gets called at the end of the control loop"""
+    def execute(self) -> None:
         if self.deployed:
-            self.shooter_motor.set(self.shoot_speed)
+            # Deploy the climber
+            pass
         else:
-            self.shooter_motor.set(0)
-
-        self.deployed = False
+            pass
