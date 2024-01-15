071724bf8b9167dae90b48f7f7f8dfe1c93ece78
Lucien Morey <lucienp@live.com.au>
2024-01-15 19:39:36 +1100

    fill in bearing calculation to face goal

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -1,6 +1,10 @@
 from magicbot import StateMachine, state
 from components.chassis import Chassis
 
+from utilities.game import get_goal_speaker_position
+
+from math import atan2
+
 
 class Shooter(StateMachine):
     chassis: Chassis
@@ -11,8 +15,13 @@ class Shooter(StateMachine):
     @state(first=True)
     def acquiring(self) -> None:
         # determine heading required for goal
-
+        translation_to_goal = (
+            get_goal_speaker_position().toTranslation2d()
+            - self.chassis.get_pose().translation()
+        )
+        bearing_to_speaker = atan2(translation_to_goal.y, translation_to_goal.x)
         # set to appropriate heading
+        self.chassis.snap_to_heading(bearing_to_speaker)
 
         # progress state machine if within tolerance
         if self.chassis.at_desired_heading():
