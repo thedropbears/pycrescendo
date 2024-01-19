10cfe7fdc74441a1f7de98116daf0b0a34c69ad7
TomSDropBear <156143379+TomSDropBear@users.noreply.github.com>
2024-01-19 15:18:44 +1100

    Create climber component framework
    This is a basic framework for the scripting used for the climber component, if this is incorrect script or there are any edits I should make please message me

--- components/climber.py
+++ components/climber.py
@@ -1,3 +1,8 @@
+import wpilib
+from rev import CANSparkMax
+from ids import SparkMaxIds
+
+
 class ClimberComponent:
     def __init__(self) -> None:
         self.deployed = False
@@ -7,7 +12,34 @@ class ClimberComponent:
 
     def execute(self) -> None:
         if self.deployed:
+
+            def __init__(self) -> None:
+                self.deployed = False
+
+    def deploy(self) -> None:
+        self.deployed = True
+
+    def execute(self) -> None:
+        if self.deployed:
             # Deploy the climber
-            pass
+            self.climbingmotor = CANSparkMax(
+                SparkMaxIds.climber, CANSparkMax.MotorType.kBrushless
+            )  # using a Neo with 4:1 4:1 3:1 ratio
+            self.limitswitch = wpilib.LimitSwitch(2)
+
+            def motorCurrent():
+                return CANSparkMax.getOutputCurrent
+
+            TriggeringLevels = 100  # TriggeringLevels Placeholder for the current levels needed to stop
+            limitswitchHit = False  # placeholder for limit switch variable
+            if limitswitchHit == False:
+                self.drive.motor(0.5, 0)
+            elif limitswitchHit == True & motorCurrent() <= TriggeringLevels:
+                self.drive.motor(-0.5, 0)
+            elif limitswitchHit == True & motorCurrent() >= TriggeringLevels:
+                self.drive.motor(0, 0)
+            else:
+                pass
         else:
             pass
+
