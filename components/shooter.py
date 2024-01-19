1cb10cce2af56a044464d83067af821b36fe8855
outsidermm <outsidermm1234@gmail.com>
2024-01-19 13:28:34 +1100

    added syntaxical annotation, setup motor controller

--- components/shooter.py
+++ components/shooter.py
@@ -1,16 +1,25 @@
-from magicbot import tunable
+from magicbot import tunable, feedback
 from rev import CANSparkMax
-from ids import SparkMaxIds, TalonIds
+from ids import SparkMaxIds, TalonIds, DioChannels
 import phoenix6
 from phoenix6.controls import VoltageOut
 import phoenix6.hardware
+from wpilib import DutyCycleEncoder
 
 
 class ShooterComponent:
     flywheel_speed = tunable(0.0)
     inject_speed = tunable(0.0)
 
-    def __init__(self):
+    # TODO Figure that out
+    MAX_INCLINE_ANGLE = 45
+    MIN_INCLINE_ANGLE = 10
+
+    def __init__(self) -> None:
+        self.inclinator = CANSparkMax(
+            SparkMaxIds.shooter_inclinator, CANSparkMax.MotorType.kBrushless
+        )
+        self.inclinator_encoder = DutyCycleEncoder(DioChannels.inclinator_encoder)
         self.flywheel = phoenix6.hardware.TalonFX(TalonIds.shooter_flywheel)
         self.injector = CANSparkMax(
             SparkMaxIds.shooter_injector, CANSparkMax.MotorType.kBrushless
@@ -19,13 +28,18 @@ class ShooterComponent:
 
         self.should_inject = False
 
-    def shoot(self):
+    def shoot(self) -> None:
         self.should_inject = True
 
-    def is_ready(self):
+    @feedback
+    def is_ready(self) -> bool:
         return True
 
-    def execute(self):
+    @feedback
+    def at_inclination(self) -> bool:
+        pass
+
+    def execute(self) -> None:
         """This gets called at the end of the control loop"""
         flywheel_request = VoltageOut(12.0 * self.flywheel_speed)
         if self.should_inject:
