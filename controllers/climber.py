dc6447b9cf4ab202ae2697f1f3afe0b00999027f
clubpenguin <amber.knights@thedropbears.org.au>
2024-01-25 20:43:12 +1100

    added stop func and removed initial call

--- controllers/climber.py
+++ controllers/climber.py
@@ -12,16 +12,17 @@ class Climber(StateMachine):
     def deploy(self) -> None:
         self.engage()
 
+    def stop(self) -> None:
+        self.done()
+
     @state(must_finish=True, first=True)
-    def extend_hook(self, initial_call: bool) -> None:
-        if initial_call:
-            self.climber_component.deploy()
-        if self.climber_component.has_deploy_finished() and self.button_pressed:
+    def extend_hook(self) -> None:
+        self.climber_component.deploy()
+        if self.climber_component.has_deploy_finished() and self.should_climb:
             self.next_state("retract_hook")
 
     @state(must_finish=True)
-    def retract_hook(self, initial_call: bool) -> None:
-        if initial_call:
-            self.climber_component.retract()
+    def retract_hook(self) -> None:
+        self.climber_component.retract()
         if self.climber_component.has_climb_finished():
             self.done()
