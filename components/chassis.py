d8500ce4cd70e8f2b1636bfe6c61ba56ffbc92cc
Michael Kennedy <34204085+mlists@users.noreply.github.com>
2024-01-25 20:25:26 +1100

    Return steer angle in radians

--- components/chassis.py
+++ components/chassis.py
@@ -134,7 +134,7 @@ class SwerveModule:
 
     def get_angle_integrated(self) -> float:
         """Gets steer angle from motor's integrated relative encoder"""
-        return self.steer.get_position().value
+        return self.steer.get_position().value * math.tau
 
     def get_rotation(self) -> Rotation2d:
         """Get the steer angle as a Rotation2d"""
