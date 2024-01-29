b4609ac77354d3e30cb46e8feb1852bf991bf814
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-29 16:38:07 +1100

    Made ALL the positions in the correct spots! Except the shooting position. I don't think that that position is even a preset spot and it's just something someone put in a random spot, correct me if I'm wrong

--- utilities/position.py
+++ utilities/position.py
@@ -11,11 +11,11 @@ class NodePosition:
 class NotePositions:
     # Order from the driver station 1-3
     # 1 is always the closest to the side of driver station 1
-    Stage1 = NodePosition(Translation2d(13.69568, 6.90365), Rotation2d(0))
-    Stage2 = NodePosition(Translation2d(13.69568, 5.45585), Rotation2d(0))
-    Stage3 = NodePosition(Translation2d(13.69568, 4.00805), Rotation2d(0))
-    Centre1 = NodePosition(Translation2d(8.270875, 7.36085), Rotation2d(0))
-    Centre2 = NodePosition(Translation2d(8.270875, 5.68445), Rotation2d(0))
+    Stage1 = NodePosition(Translation2d(13.69568, 7.00045), Rotation2d(0))
+    Stage2 = NodePosition(Translation2d(13.69568, 5.55265), Rotation2d(0))
+    Stage3 = NodePosition(Translation2d(13.69568, 4.1057), Rotation2d(0))
+    Centre1 = NodePosition(Translation2d(8.270875, 7.4585), Rotation2d(0))
+    Centre2 = NodePosition(Translation2d(8.270875, 5.7821), Rotation2d(0))
     Centre3 = NodePosition(Translation2d(8.270875, 4.1057), Rotation2d(0))
     Centre4 = NodePosition(Translation2d(8.270875, 2.4293), Rotation2d(0))
     Centre5 = NodePosition(Translation2d(8.270875, 0.75286), Rotation2d(0))
