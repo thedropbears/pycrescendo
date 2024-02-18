530ec4668024b3e160f8eb30e3e3ffcf2010d76e
Michael Kennedy <34204085+mlists@users.noreply.github.com>
2024-02-19 06:51:18 +1100

    Fill in note manager

--- controllers/note.py
+++ controllers/note.py
@@ -1,4 +1,4 @@
-from magicbot import StateMachine, state
+from magicbot import StateMachine, state, will_reset_to
 
 from components.injector import InjectorComponent
 from components.intake import IntakeComponent
@@ -12,30 +12,61 @@ class NoteManager(StateMachine):
     injector_component: InjectorComponent
     intake: IntakeComponent
 
+    shot_desired = will_reset_to(False)
+    intake_desired = will_reset_to(False)
+    intake_cancel_desired = will_reset_to(False)
+
     def __init__(self):
         pass
 
     def try_intake(self):
-        pass
+        self.intake_desired = True
 
     def cancel_intake(self):
-        pass
+        self.intake_cancel_desired = True
 
     def try_shoot(self):
-        pass
+        self.shot_desired = True
 
     @state(first=True, must_finish=True)
-    def idling(self):
-        pass
+    def idling(self, initial_call):
+        if initial_call:
+            self.intake.retract()
+
+        if self.intake_desired:
+            self.next_state(self.dropping_intake)
+
+    @state(must_finish=True)
+    def dropping_intake(self, inital_call):
+        if inital_call:
+            self.intake.deploy()
+
+        if self.intake.is_fully_deployed():
+            self.next_state(self.intaking)
 
     @state(must_finish=True)
     def intaking(self):
-        pass
+        self.intake.intake()
+        self.injector_component.intake()
+
+        if self.injector_component.has_note():
+            self.next_state(self.holding_note)
+
+        if self.intake_cancel_desired:
+            self.next_state(self.idling)
 
     @state(must_finish=True)
-    def holding_note(self):
-        pass
+    def holding_note(self, initial_call):
+        if initial_call:
+            self.intake.retract()
+        if self.shot_desired:
+            self.next_state(self.trying_to_shoot)
 
     @state(must_finish=True)
     def trying_to_shoot(self):
-        pass
+        self.shooter.shoot()
+        if self.shooter.has_just_fired():
+            self.next_state(self.idling)
+
+        if not self.shot_desired:
+            self.next_state(self.holding_note)
