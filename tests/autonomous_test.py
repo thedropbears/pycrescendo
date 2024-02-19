67f3dedc20e883b5d6ff54be7778305ea54590e4
David Vo <auscompgeek@users.noreply.github.com>
2024-02-19 21:11:17 +1100

    tests: Skip auto and fuzz tests if basic robot tests fail (#125)

--- tests/autonomous_test.py
+++ tests/autonomous_test.py
@@ -6,6 +6,7 @@ from robotpy_ext.autonomous.selector_tests import (  # type: ignore[import-untyp
 from wpilib.simulation import DriverStationSim
 
 
+@pytest.mark.slow_integration_test
 @pytest.mark.parametrize("alliance", ["Red", "Blue"])
 def test_all_autonomous(control, alliance):
     station = getattr(hal.AllianceStationID, f"k{alliance}1")
