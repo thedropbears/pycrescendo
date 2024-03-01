7edf7f0d79906822b16ee9b9ce64d25aa3779ffa
Michael Kennedy <34204085+mlists@users.noreply.github.com>
2024-03-01 18:17:21 +1100

    Adjust camera position

--- robot.py
+++ robot.py
@@ -55,13 +55,13 @@ class MyRobot(magicbot.MagicRobot):
         self.lights_strip_length = 144  # TODO Change to correct length
 
         self.vision_port_name = "ardu_cam_port"
-        self.vision_port_pos = Translation3d(0.287, 0.055, 0.27)
+        self.vision_port_pos = Translation3d(0.005, 0.161, 0.503)
         self.vision_port_rot = Rotation3d(
             0, -math.radians(20), math.radians(180) - math.radians(90 - 71.252763)
         )
 
         self.vision_starboard_name = "ardu_cam_starboard"
-        self.vision_starboard_pos = Translation3d(0.287, -0.058, 0.27)
+        self.vision_starboard_pos = Translation3d(0.005, 0.221, 0.503)
         self.vision_starboard_rot = Rotation3d(
             0, -math.radians(20), math.radians(180) + math.radians(90 - 71.252763)
         )
