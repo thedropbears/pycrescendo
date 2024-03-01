17ce60da7af0f3b5852e881789e0dc038c4e4b6e
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-03-02 10:25:04 +1100

    Added wpilib check to only retract if not in Autonomous

--- controllers/note.py
+++ controllers/note.py
@@ -1,4 +1,5 @@
 from magicbot import StateMachine, state, feedback, will_reset_to
+import wpilib
 
 from components.intake import IntakeComponent
 from controllers.shooter import Shooter
@@ -59,7 +60,7 @@ class NoteManager(StateMachine):
         if self.intake_desired:
             self.intake.deploy()
             self.intake.intake()
-        else:
+        elif not wpilib.DriverStation.isAutonomous():
             self.intake.retract()
 
         if self.has_note():
