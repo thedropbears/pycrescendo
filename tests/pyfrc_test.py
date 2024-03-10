912215c03bab6f5d92c47a1637b5a0ad731fac13
David Vo <auscompgeek@users.noreply.github.com>
2024-03-10 14:37:38 +1100

    pyfrc_test: Disable test_practice
    This takes 14s on my laptop.
    
    In practice, our fuzz tests likely hit more code paths than this test
    which simply has the robot sit in one spot.

--- tests/pyfrc_test.py
+++ tests/pyfrc_test.py
@@ -2,12 +2,10 @@
 from pyfrc.tests import (
     test_disabled,
     test_operator_control,
-    test_practice,
 )
 
 # Make pyflakes happy about our imports.
 __all__ = (
     "test_disabled",
     "test_operator_control",
-    "test_practice",
 )
