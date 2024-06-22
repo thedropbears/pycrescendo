e861368c50ae32278956393bda8ee0bd97c626a4
Beana1234 <asher.broadhead@thedropbears.org.au>
2024-06-22 17:32:41 +1000

    retune lookup

--- components/shooter.py
+++ components/shooter.py
@@ -44,8 +44,8 @@ class ShooterComponent:
     # Add extra point outside our range to ramp speed down to zero
     FLYWHEEL_DISTANCE_LOOKUP = (0, 1.3, 2.0, 3.0, 4.0, 5.0, 7.0)
     FLYWHEEL_SPEED_LOOKUP = (
-        57,
-        57,
+        54,
+        54,
         60,
         64,
         65,
@@ -53,9 +53,9 @@ class ShooterComponent:
         0,
     )
     FLYWHEEL_ANGLE_LOOKUP = (
-        1.0,
-        1.0,
-        0.69,
+        0.93,
+        0.93,
+        0.77,
         0.57,
         0.49,
         0.46,
